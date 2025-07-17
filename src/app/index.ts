edcbcaa3d3a19006988d7e4f56a6e02ea3fce679
sambit24795 <50554112+sambit24795@users.noreply.github.com>
2025-07-17 23:54:08 +0530

    added nginx route redirtection from localhost to www.fulcrum.com

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application } from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan, { StreamOptions } from 'morgan';
